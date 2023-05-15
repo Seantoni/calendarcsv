e4de9bf780f356b31ab241e51a4239760cea8c61
Seantoni <101011785+Seantoni@users.noreply.github.com>
2023-05-14 21:00:13 -0500

    Update script.js reert back

--- script.js
+++ script.js
@@ -1,78 +1,75 @@
-document.getElementById("calculate").addEventListener("click", () => {
-  const inputFile = document.getElementById("inputFile").files[0];
-  if (!inputFile) {
-    alert("Please select a CSV file");
-    return;
-  }
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('date-form');
+    const startDate = document.getElementById('start-date');
+    const endDate = document.getElementById('end-date');
+    const daysOfWeek = document.querySelectorAll('input[name="day-of-week"]');
+    const selectAll = document.getElementById('select-all');
+    const optionName = document.getElementById('option-name');
+    const availability = document.getElementById('availability');
 
-  Papa.parse(inputFile, {
-    complete: processCSV,
-    error: () => {
-      alert("Input file format is not valid");
-    }
+    // Add event listener to "Select All" checkbox
+    selectAll.addEventListener('change', () => {
+      const isChecked = selectAll.checked;
+      daysOfWeek.forEach(dayOfWeek => {
+        dayOfWeek.checked = isChecked;
+      });
+    });
+
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      // Get the start and end dates from the input fields
+      const start = new Date(startDate.value);
+      const end = new Date(endDate.value);
+
+      // Get an array of the selected days of the week
+      const selectedDaysOfWeek = [];
+      daysOfWeek.forEach(dayOfWeek => {
+        if (dayOfWeek.checked && dayOfWeek.value !== 'all') {
+          selectedDaysOfWeek.push(dayOfWeek.value);
+        }
+      });
+
+      // Generate the dates for the selected days of the week
+      const dates = generateDates(start, end, selectedDaysOfWeek);
+
+      // Calculate the availability value
+      const totalAvailability = Number(availability.value);
+      const rows = dates.length;
+      const availPerRow = Math.floor(totalAvailability / rows);
+
+      // Download the CSV file
+      downloadCSV(dates, optionName.value, availPerRow);
+    });
   });
-});
 
-function removeCommas(str) {
-  return str.replace(/,/g, "");
-}
+  function generateDates(start, end, selectedDaysOfWeek) {
+    const dates = [];
+    const oneDay = 24 * 60 * 60 * 1000;
 
-function processCSV(results) {
-  const data = results.data.slice(1).map((row) => ({
-    "business name": row[2],
-    id: +removeCommas(row[0]),
-    "vouchers sold": +removeCommas(row[9]),
-    "Sold per day": +removeCommas(row[13]),
-    "net revenue": +removeCommas(row[10]),
-    "conversion rate": +removeCommas(row[12])
-  }));
-
-  const columns = [
-    "vouchers sold",
-    "Sold per day",
-    "net revenue",
-    "conversion rate"
-  ];
-  const factorWeights = [0.25, 0.1, 0.25, 0.4];
-
-  for (const column of columns) {
-    const minVal = Math.min(...data.map((row) => row[column]));
-    const maxVal = Math.max(...data.map((row) => row[column]));
-
-    for (const row of data) {
-      row[column] =
-        minVal === maxVal ? 0 : (row[column] - minVal) / (maxVal - minVal);
+    for (let d = new Date(start); d <= end; d.setDate(d.getDate() + 1)) {
+      if (selectedDaysOfWeek.includes(d.getDay().toString())) {
+        const dateObj = { date: formatDate(d), day: d.toLocaleString('en-US', { weekday: 'long' }) };
+        dates.push(dateObj);
+      }
     }
+
+    return dates;
   }
 
-  for (const row of data) {
-    row["Deal Score"] = columns.reduce(
-      (acc, column, index) => acc + row[column] * factorWeights[index],
-      0
-    );
+  function formatDate(date) {
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear();
+    return `${year}-${month < 10 ? '0' : ''}${month}-${day < 10 ? '0' : ''}${day}`;
   }
 
-  data.sort((a, b) => b["Deal Score"] - a["Deal Score"]);
-
-  const outputData = [
-    ["Rank", "business name", "id", "Deal Score"],
-    ...data.map((row, index) => [
-      index + 1,
-      row["business name"],
-      row["id"],
-      row["Deal Score"]
-    ])
-  ];
-  
-  const outputCSV = Papa.unparse(outputData);
-
-  const csvBlob = new Blob([outputCSV], { type: "text/csv" });
-  const csvUrl = URL.createObjectURL(csvBlob);
-
-  const tempLink = document.createElement("a");
-  tempLink.href = csvUrl;
-  tempLink.download = "output_file.csv";
-  document.body.appendChild(tempLink);
-  tempLink.click();
-  document.body.removeChild(tempLink);
-}
+  function downloadCSV(dates, optionName, availPerRow) {
+    const filename = `${optionName}.csv`;
+    const csvContent = 'data:text/csv;charset=utf-8,Date,Day,Disponibilidad\n' + dates.map(dateObj => `${dateObj.date},${dateObj.day},${availPerRow}`).join('\n');
+    const encodedUri = encodeURI(csvContent);
+    const link = document.createElement('a');
+    link.setAttribute('href', encodedUri);
+    link.setAttribute('download', filename);
+    link.click();
+  }
